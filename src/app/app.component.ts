0b8242e8a098dca1c66a7f5bb8fd9c26f9016a19
cayetananevado321@gmail.com <cayetananevado321@gmail.com>
2024-03-03 23:15:01 -0600

    Mi piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'PIANO1';
+  AplicarSonido(numero: number): void
+{
+  let audio = new Audio();
+  switch (numero) {
+    case 1:
+      audio.src = '../assets/Sonidos/do-80236.mp3';
+      audio.load();
+      audio.play();
+      break;
+      case 2:
+      audio.src = '../assets/Sonidos/re-78500.mp3';
+      audio.load();
+      audio.play();
+      break;
+      case 3:
+      audio.src = '../assets/Sonidos/mi-80239.mp3';
+      audio.load();
+      audio.play();
+      break;
+      case 4:
+      audio.src = '../assets/Sonidos/fa-78409.mp3';
+      audio.load();
+      audio.play();
+      break;
+      case 5:
+      audio.src = '../assets/Sonidos/sol-101774.mp3';
+      audio.load();
+      audio.play();
+      break;
+      case 6:
+      audio.src = '../assets/Sonidos/si-80238.mp3';
+      audio.load();
+      audio.play();
+      break;
+}
+  }
+  
 }
